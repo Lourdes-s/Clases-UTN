b970edfa9fa0afd660d1222cf20ab10b9c527db3
Lourdes.s <lou_santillan98@hotmail.com>
2024-06-28 19:02:10 -0300

    clase 19

---
+++ clase-19/script.js
@@ -0,0 +1,12 @@
+/* console .dir(document) */
+
+/* 
+document es una variable global de JS 
+la variable document es una representacion como objeto de nuestro html 
+el 'console.dir' es lo mismo que el 'console.log' solamente que muestra los objetos como objetos, en cambio el console.log me lo muestra como html en la consola 
+ */
+
+
+
+/* GET ELEMENT BY ID  */
+/* es un metodo que nos permite tener un elemento por si id */
