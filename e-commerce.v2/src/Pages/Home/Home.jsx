40c2fd8b4ab3ef2cfc1d6a808dcab10dbcebb969
Lourdes.s <lou_santillan98@hotmail.com>
2024-08-01 00:50:14 -0300

    e-commerce

--- e-commerce.v2/src/Pages/Home/Home.jsx
+++ e-commerce.v2/src/Pages/Home/Home.jsx
@@ -6,54 +6,53 @@ import { obtenerProductos } from '../../Fetching/products.fetching'
 
 
 const Home = () => {
-
-    setTimeout(
-        () => {
-            console.log('hola')
-        },
-        2000 /* esta expresado en ms(en este caso son 2seg) */
-    ) /* ejecuta una funcion despues de cierto tiempo */
-
     useEffect(
         () => {
-            /* aca va la funcionalidad que queremos controlar, es decir que posterga la ejecucion de la callback */
-
-
-            obtenerProductos()
-            .then(
-                (productos) => {
-                    console.log('productos', productos)
-                    setProductsList(productos) /*  cuando cambio un estado se vuelve a renderizar el componente, lo que significa que se vuelve un bucle */
-                }
-            )
+            /* USE EFFECT aca va la funcionalidad que queremos controlar, es decir que posterga la ejecucion de la callback */
+            setTimeout(
+                () => {
+                    obtenerProductos()
+                    .then(
+                        (productos) => {
+                            console.log('productos', productos)
+                            setProductsList(productos) /*  cuando cambio un estado se vuelve a renderizar el componente, lo que significa que se vuelve un bucle infinito */
+                            setIsLoading(false)
+                        }
+                    )
+                },
+                2000 /* estoy haciendo un delay manual, para emular la carga del fetch como lo haria en un contexto normal */
+            ) 
         },
         [] /* array vacio en un efecto significa que solo se va a cargar una vez */
     )
 
-
     const [productsList, setProductsList] = useState([])
-
+    const [isLoading, setIsLoading] = useState(true)
     return (
         <div>
             <Navbar/>
             <h1>Lista de productos</h1>
             <div>
-                {productsList.map(product => {
-                    return (
-                        <div key={product.id}>
-                            <h2>{product.titulo}</h2>
-                            <span>Precio: ${product.precio}</span>
-                            <Link to={'/detail/' + product.id}>ver detalle<BiChevronRightCircle className='icon' /></Link> {/* para darle estilos le puedo poner una class */}
-                            <hr/>
-                        </div>
-                    )
-                })}
+                {
+                    isLoading
+                    ? <h2>Cargando</h2>
+                    : productsList.map(product => {
+                        return (
+                            <div key={product.id}>
+                                <h2>{product.titulo}</h2>
+                                <span>Precio: ${product.precio}</span>
+                                <Link to={'/detail/' + product.id}>ver detalle<BiChevronRightCircle className='icon' /></Link> {/* para darle estilos le puedo poner una class */}
+                                <hr/>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
 }
 
-/*---------------------------------------------- THEN/CATCH---------------------------------------------- */
+/*----------------------------------------------THEN/CATCH----------------------------------------------*/
 
 /* then catch son metodos que nos van a permitir controlar que va a pasar cuando se resuelva obtener productos */
     /* then se va a ejecutar cuando la prmesa sea resuelta y todo este correcto. va a ejecutar una callback que se va a resolver cuando se termine de ejecutar la promesa then por parametro va a recibir el retorno de la funcion asincronica */
@@ -63,12 +62,26 @@ const Home = () => {
     })
      */
 
-/*---------------------------------------------- EFECTOS---------------------------------------------- */
+/*----------------------------------------------EFECTOS----------------------------------------------*/
 
 /* los efectos nos permiten elegir cuantas veces se va a recargar una funcionalidad, si el estado nos permite saber cuantas veces se recarga o nos permite controlar la recarga de un componente, los efectos nos permite cambiar la funcionalidad*/
 /* es un nuevo hook llamado useEffect */
 /* recibe 2 parametros, es una funcion que recibe dos parametros, el primer parametro es una callback, el segundo parametro es un array */
 /* el primer parametro que recibe, es decir la callback es el mismo efecto */
+
+/*----------------------------------------------SET TIME OUT----------------------------------------------*/
+/* ejecuta una funcion despues de cierto tiempo, es decir que posterga la ejecusion de una callback */
+/* esta expresado en ms(en este caso son 2seg), es decir que se ejecuta el hola despues de 2 segundos */
+/* 
+setTimeout(
+    () => {
+        console.log('hola')
+    },
+    2000 
+) 
+*/
+
+/*----------------------------------------------SET INTERVALS----------------------------------------------*/
 
 
 export default Home
