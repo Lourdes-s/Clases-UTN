8f1ced91f3d12bb3f752ee5c1c8250a725af4c0c
Lourdes.s <lou_santillan98@hotmail.com>
2024-08-03 08:35:10 -0300

    e-commerce

--- e-commerce.v2/src/Pages/Home/Home.jsx
+++ e-commerce.v2/src/Pages/Home/Home.jsx
@@ -6,6 +6,12 @@ import { obtenerProductos } from '../../Fetching/products.fetching'
 
 
 const Home = () => {
+
+    const [productsList, setProductsList] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [searchString, setSearchString] = useState('')
+    const [isWriting, setIsWriting] = useState(false)
+
     useEffect(
         () => {
             /* USE EFFECT aca va la funcionalidad que queremos controlar, es decir que posterga la ejecucion de la callback */
@@ -15,23 +21,54 @@ const Home = () => {
                     .then(
                         (productos) => {
                             console.log('productos', productos)
-                            setProductsList(productos) /*  cuando cambio un estado se vuelve a renderizar el componente, lo que significa que se vuelve un bucle infinito */
+                            if(searchString){
+                                const nuevaListaProductos = productos.filter(
+                                    producto => 
+                                        producto.titulo.toLowerCase().includes(searchString.toLowerCase()) 
+                                )
+                                setProductsList(nuevaListaProductos)
+                            }
+                            else{
+                                setProductsList(productos)
+                            }
                             setIsLoading(false)
                         }
                     )
                 },
-                2000 /* estoy haciendo un delay manual, para emular la carga del fetch como lo haria en un contexto normal */
+                500 /* estoy haciendo un delay manual, para emular la carga del fetch como lo haria en un contexto normal */
             ) 
         },
-        [] /* array vacio en un efecto significa que solo se va a cargar una vez */
+        [searchString] /* array vacio en un efecto significa que solo se va a cargar una vez */
+        /* agregandole el searchString hace que cada vez que yo escriba algo se vuelva a ejecutar el efecto */
     )
 
-    const [productsList, setProductsList] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+
+
+    const handleChangeValue = (e) => {
+        console.log(e.target.value)
+        setSearchString(e.target.value)/* porque cuando escribo no se ve lo que escribo en mi input, entonces seteo como valor el e.target.value entonces lo que yo escriba se va a guardar en el estado de searchString, deja de ser comillas vacias*/
+    }
+    const handlechangeFocus = () => {
+        setIsWriting(true)
+    }
+    const handleAbortFocus = () => {
+        setIsWriting(false)
+    } 
+
     return (
         <div>
             <Navbar/>
+            {isWriting && <h2>Escribiendo...</h2>}
             <h1>Lista de productos</h1>
+            <input
+            placeholder='buscar producto' 
+            onChange={handleChangeValue} 
+            value={searchString} 
+            onFocus={handlechangeFocus}
+            onBlur={handleAbortFocus}
+            /> 
+            {/* al valor del input le agregas el valor del estado, el en onchage pasas la referencia de la funcion*/}
+            {searchString != '' ? <h2>Escribiendo</h2> : <h2>Esperandocambios</h2>}
             <div>
                 {
                     isLoading
@@ -66,8 +103,9 @@ const Home = () => {
 
 /* los efectos nos permiten elegir cuantas veces se va a recargar una funcionalidad, si el estado nos permite saber cuantas veces se recarga o nos permite controlar la recarga de un componente, los efectos nos permite cambiar la funcionalidad*/
 /* es un nuevo hook llamado useEffect */
-/* recibe 2 parametros, es una funcion que recibe dos parametros, el primer parametro es una callback, el segundo parametro es un array */
+/* es una funcion que recibe dos parametros, el primer parametro es una callback, el segundo parametro es un array */
 /* el primer parametro que recibe, es decir la callback es el mismo efecto */
+/* cuando cambio un estado se vuelve a renderizar el componente, lo que significa que se vuelve un bucle infinito */
 
 /*----------------------------------------------SET TIME OUT----------------------------------------------*/
 /* ejecuta una funcion despues de cierto tiempo, es decir que posterga la ejecusion de una callback */
